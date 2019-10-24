93f034ff2eb7b1bf3f1b120e0a446501be656b70
pc619 <pc619@ic.ac.uk>
2019-10-24 14:01:36 +0100

    downloaded week 4

---
+++ Week-4/README.md
@@ -0,0 +1,49 @@
+# Week 4 Exercises
+
+# Tuesday - More on Datatypes
+
+
+## Manipulating Data Structures
+
+1. Open up `structures.py` and follow the instructions in the comments above each function definition.
+
+2. Run the test file to determine if the function work correctly.
+
+```
+python -m unittest test_structures.py
+```
+
+## Docstrings
+
+1. Add docstrings to your functions in `structures.py`.
+
+2. Import your module `structures.py` in an interactive Python shell. Check that your docstrings print when calling the `help()` function.
+
+```
+import structures as s
+help(s.first_and_last)
+```
+
+## Linting
+
+1. Run pylint on the file `fuzzy.py` and correct the issues that are printed.
+
+```
+pylint fuzzy.py
+```
+
+2. Run pylint on your `fuzzy.py` and fix the issues.
+
+# Thursday - More on Functions
+
+## Default and Keyword Values
+
+1. Open up `keywords.py` and follow the instructions in the comments.
+
+2. Run the test file to determine if the functions work correctly.
+
+```
+python -m unittest test_keywords.py
+```
+
+3. Run your code through PyLint and correct as any issues.
---
+++ Week-4/test_structures.py
@@ -0,0 +1,129 @@
+'''
+test_structures.py
+
+Unit tests for structures functions.
+'''
+
+
+import unittest
+import unittest.mock
+import io
+
+import structures as st
+
+class TestStructures(unittest.TestCase):
+    def set_up(self):
+        pass
+
+
+    def test_first_and_last(self):
+        '''
+        Test first_and_last function
+        '''
+        result = st.first_and_last([0,1,2,3])
+        self.assertEqual(result, [0,3])
+
+    def test_part_reverse(self):
+        '''
+        Test
+        '''
+        result = st.part_reverse([0,1,2,3,4,5], 1, 4)
+        self.assertEqual(result, [3,2,1])
+
+    def test_repeat_at_index(self):
+        '''
+        Test 
+        '''
+        result = st.repeat_at_index([0,1,2,3,4,5,6], 3)
+        self.assertEqual(result, [0,1,2,3,3,3,4,5,6])
+
+    def test_palindrome_word1(self):
+        '''
+        Test palindrome word
+        '''
+        result = st.palindrome_word("madam")
+        self.assertIs(result, True)
+
+    def test_palindrome_word2(self):
+        '''
+        Test palindrome word
+        '''
+        result = st.palindrome_word("Madam")
+        self.assertIs(result, True)
+
+    def test_palindrome_word3(self):
+        '''
+        Test palindrome word
+        '''
+        result = st.palindrome_word("palindrome")
+        self.assertIs(result, False)
+
+
+    def test_palindrome_sentence1(self):
+        '''
+        Test palindrome sentence
+        '''
+        result = st.palindrome_sentence("Was it a car or a cat I saw")
+        self.assertIs(result, True)
+
+    def test_palindrome_sentence2(self):
+        '''
+        Test palindrome sentence
+        '''
+        result = st.palindrome_sentence("Random sentence")
+        self.assertIs(result, False)
+
+    def test_palindrome_sentence3(self):
+        '''
+        Test palindrome sentence
+        '''
+        result = st.palindrome_sentence(" Do geese see God  ")
+        self.assertIs(result, True)
+
+    def test_concatenate_sentences1(self):
+        '''
+        Test sentence concatenation
+        '''
+        result = st.concatenate_sentences("First sentence.", "Second sentence.")
+        self.assertEqual(result, "First sentence. Second sentence.")
+
+     #def test_concatenate_sentences2(self):
+     #   '''
+     #   Test sentence concatenation
+     #   '''
+     #   result = st.concatenate_sentences("first sentence.", "Second sentence.")
+     #   self.assertRaises(ValueError,
+
+    def test_index_exists1(self):
+        '''
+        Test 
+        '''
+        result = st.index_exists({"ind1": "val1", "ind2": "val2"} , "ind1")
+        self.assertIs(result, True)
+
+
+    def test_value_exists1(self):
+        '''
+        Test 
+        '''
+        result = st.value_exists({"ind1": "val1", "ind2": "val2"} , "val1")
+        self.assertIs(result, True)
+
+    def test_index_exists2(self):
+        '''
+        Test 
+        '''
+        result = st.value_exists({"ind1": "val1", "ind2": "val2"} , "val3")
+        self.assertIs(result, False)
+
+
+    def test_merge_dictionaries(self):
+        '''
+        Test
+        '''
+        result = st.merge_dictionaries({"a": 1, "c": 3}, {"b": 2, "d": 4})
+        self.assertEqual(result, {"a": 1, "c": 3, "b": 2, "d": 4})
+
+
+if __name__ == '__main__':
+    unittest.main()
