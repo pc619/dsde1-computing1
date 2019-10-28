ebef5f26067119f124ed891733ab3f6bfb36e249
pc619 <pc619@ic.ac.uk>
2019-10-28 22:45:44 +0000

    question 1

--- practice-assessment-1-pc619-master/question1/question1.py
+++ practice-assessment-1-pc619-master/question1/question1.py
@@ -5,5 +5,23 @@ Implementation of the flowchart in question1.png.
 '''
 
 def flowchart(input_value):
-    return
+    
+    returnMessage = ""
+
+    if input_value==0:
+       returnMessage = "zero"
+    elif input_value>0:
+        if input_value%2==0:
+            returnMessage = "positive-even"
+        else:
+            returnMessage = "positive-odd"
+    elif input_value<0:
+        if input_value%2==0:
+            returnMessage = "negative-even"
+        else:
+            returnMessage = "negative-odd"
+    else:
+        returnMessage = "enter a valod parameter"
+    
+    return returnMessage
 
